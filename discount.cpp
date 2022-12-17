970e73b0239c7a1276d8fbd3269c83f875f063a4
shreya1881 <2704shreya@gmail.com>
2022-12-17 09:33:20 +0530

    first

---
+++ discount.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a=100;
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int b;
+	    cin>>b;
+	    cout<<(a-b)<<endl;
+	}
+	return 0;
+}
