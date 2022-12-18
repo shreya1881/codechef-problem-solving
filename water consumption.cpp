c101f80650e86871f3283abfd5d9710142181d21
shreya1881 <2704shreya@gmail.com>
2022-12-18 09:03:54 +0530

    .

---
+++ water consumption.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a;
+	    cin>>a;
+	    if(a>=2000)
+	    cout<<"YES\n";
+	    else cout<<"NO\n";
+	}
+	return 0;
+}
