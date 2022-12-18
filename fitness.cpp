2adc61e15fa9d0149059fc459c8ca995ba507f65
shreya1881 <2704shreya@gmail.com>
2022-12-18 09:08:04 +0530

    .

---
+++ fitness.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a;
+	    cin>>a;
+	    cout<<2*a*5<<endl;
+	}
+	return 0;
+}
