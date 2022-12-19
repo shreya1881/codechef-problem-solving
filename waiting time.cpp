52252336869b3a32fdfc80be124b00e6db24123a
shreya1881 <2704shreya@gmail.com>
2022-12-19 18:40:24 +0530

    .

---
+++ waiting time.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int k,x;
+	    cin>>k>>x;
+	    int a=k*7;
+	    cout<<a-x<<endl;
+	}
+	return 0;
+}
