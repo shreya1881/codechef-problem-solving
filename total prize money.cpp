dfd4aed3ee58f5907c6e39c1899a1c6c3b7ef59f
shreya1881 <2704shreya@gmail.com>
2022-12-19 18:15:42 +0530

    .

---
+++ total prize money.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    int a=x*10;
+	    int b=90*y;
+	    cout<<a+b<<endl;
+	}
+	return 0;
+}
