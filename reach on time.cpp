7ed539e1e33f50907d95a154f89325478c57e63f
shreya1881 <2704shreya@gmail.com>
2022-12-19 11:07:42 +0530

    .

---
+++ reach on time.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a;
+	    cin>>a;
+	    if(a<30)
+	    cout<<"NO\n";
+	    else
+	    cout<<"YES\n";
+	}
+	return 0;
+}
