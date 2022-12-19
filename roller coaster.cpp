26950dbd0a0f84671a998c53be12ae6fccbb97df
shreya1881 <2704shreya@gmail.com>
2022-12-19 11:20:29 +0530

    .

---
+++ roller coaster.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,h;
+	    cin>>x>>h;
+	    if(x>=h)
+	    cout<<"YES\n";
+	    else
+	    cout<<"NO\n";
+	}
+	return 0;
+}
