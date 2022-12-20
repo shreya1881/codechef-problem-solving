25600a99d1831ff4576c3f199a63bce956c301f2
shreya1881 <2704shreya@gmail.com>
2022-12-20 18:40:19 +0530

    .

---
+++ chess time.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a;
+	    cin>>a;
+	    int b=a*60;
+	    cout<<b/20<<endl;
+	}
+	return 0;
+}
