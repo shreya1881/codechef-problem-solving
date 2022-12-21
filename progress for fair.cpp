23844b26d1e2343b64c8cd315532626db810f1ea
shreya1881 <2704shreya@gmail.com>
2022-12-21 19:05:00 +0530

    .

---
+++ progress for fair.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,k;
+	    cin>>n>>k;
+	    if(n<k)
+	    cout<<"YES\n";
+	    else
+	    cout<<"NO\n";
+	}
+	return 0;
+}
