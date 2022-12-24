c09f8bf4e7c5578612f34f52183424fb642916cb
shreya1881 <2704shreya@gmail.com>
2022-12-24 11:33:53 +0530

    .

---
+++ turbo sort.cpp
@@ -0,0 +1,14 @@
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	vector <int> a(t);
+	for(int i = 0; i< t ; i++){
+	    cin>>a[i];
+	}
+	sort(a.begin(),a.end());
+	for(int x : a)
+	    cout<<x<<endl;
+	return 0;
+}
+
