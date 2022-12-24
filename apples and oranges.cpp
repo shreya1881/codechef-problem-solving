c82ffe1bc77254e58f0603b9ef7dbfa9c1431e6c
shreya1881 <2704shreya@gmail.com>
2022-12-24 10:12:32 +0530

    .

---
+++ apples and oranges.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	cin>>x;
+	int a,b;
+	cin>>a>>b;
+	if(a+b<=x)
+	cout<<"Yes";
+	else cout<<"No";
+	return 0;
+}
