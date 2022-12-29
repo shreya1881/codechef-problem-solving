da55842096c97410b01636ca6c7fd8c91f736e2d
shreya1881 <2704shreya@gmail.com>
2022-12-29 10:14:06 +0530

    .

---
+++ perfect trio.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a[3];
+	    for(int i=0; i<3; i++)
+	    cin>>a[i];
+	    sort(a,a+3);
+	    if((a[0]+a[1])==a[2])
+	    cout<<"YES\n";
+	    else
+	    cout<<"NO\n";
+	}
+	return 0;
+}
