f1ffa0fa3cd6ace53d31304daf1bd7ee775f8d35
shreya1881 <2704shreya@gmail.com>
2022-12-31 12:15:31 +0530

    .

---
+++ Battery low.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x<=15)
+	    cout<<"YES\n";
+	    else cout<<"No\n";
+	}
+	return 0;
+}
