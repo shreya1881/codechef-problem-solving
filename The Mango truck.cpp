cad97d8ce28cb5641b1d63521bfbd566d2d4e1b5
shreya1881 <2704shreya@gmail.com>
2023-01-01 10:44:26 +0530

    .

---
+++ The Mango truck.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y,z;
+	    cin>>x>>y>>z;
+	    int a=z-y;
+	    cout<<a/x<<endl;
+	}
+	return 0;
+}
