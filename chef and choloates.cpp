5ac9b249ece284bf9feea14c9ada2df7e8441b6b
shreya1881 <2704shreya@gmail.com>
2023-01-02 10:24:46 +0530

    .

---
+++ chef and choloates.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int c,x,y;
+	    cin>>c>>x>>y;
+	    int a=c-x;
+	    cout<<a*y<<endl;
+	}
+	return 0;
+}
