2109592299a1a332c95b2fec72c2a8f1ec06ff4a
shreya1881 <2704shreya@gmail.com>
2023-01-02 10:23:05 +0530

    .

---
+++ playlist.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,x;
+	    cin>>n>>x;
+	    int a=x*3;
+	    cout<<n/a<<endl;
+	}
+	return 0;
+}
