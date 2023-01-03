603f7259354b2c3bdb74422ff8a2c2427520c438
shreya1881 <2704shreya@gmail.com>
2023-01-03 10:31:36 +0530

    .

---
+++ chef in his office.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    int a=x*4;
+	    cout<<a+y<<endl;
+	}
+	return 0;
+}
