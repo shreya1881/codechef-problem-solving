da0366372d45631d935da516f2074296ecc1c382
shreya1881 <2704shreya@gmail.com>
2023-01-03 10:26:08 +0530

    .

---
+++ Janmansh and assignments.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    int a=3+x;
+	    if(a>10) cout<<"No\n";
+	    else cout<<"Yes\n";
+	}
+	return 0;
+}
