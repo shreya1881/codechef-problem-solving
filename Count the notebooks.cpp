885c702a9a64b412cca6c30681adb2d51d16ee5e
shreya1881 <2704shreya@gmail.com>
2023-01-04 09:49:53 +0530

    .

---
+++ Count the notebooks.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int pages = n*1000;
+	    cout<<pages/100<<endl;
+	}
+	return 0;
+}
