2a2701cb26af8c7d91cd58be486d1566ed0b5c2a
shreya1881 <2704shreya@gmail.com>
2023-01-05 21:45:09 +0530

    .

---
+++ The three topics.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b,c,x;
+	cin>>a>>b>>c>>x;
+	if(x==a||x==b||x==c) cout<<"yes\n";
+	else cout<<"No\n";
+	return 0;
+}
