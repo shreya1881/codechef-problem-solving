6325d6f6890b6f79878c053ae1afb20b52a202e4
shreya1881 <2704shreya@gmail.com>
2023-01-06 09:46:25 +0530

    .

---
+++ chef and Bird Farm.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int t;
+cin>>t;
+while(t>0){
+ int x,y,z;
+ cin>>x>>y>>z;
+ if(z%x==0&&z%y==0)
+ cout<<"ANY"<<endl;
+ else if(z%x==0)
+ cout<<"CHICKEN"<<endl;
+ else if(z%y==0)
+ cout<<"DUCK"<<endl;
+ else
+ cout<<"NONE"<<endl;
+    
+    
+t--;
+}
+    
+	return 0;
+}
