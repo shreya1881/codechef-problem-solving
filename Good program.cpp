0d81452ba219891a349f77ce412895ac126c805b
shreya1881 <2704shreya@gmail.com>
2023-01-07 18:01:56 +0530

    .

---
+++ Good program.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    if(n%4==0) cout<<"Good\n";
+	    else cout<<"NOT Good\n";
+	}
+	return 0;
+}
