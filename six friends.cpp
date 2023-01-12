34ac71a2d2f03fb6a4f17e589eabe988d21bf9c3
shreya1881 <2704shreya@gmail.com>
2023-01-12 09:45:25 +0530

    .

---
+++ six friends.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    int a=3*x;
+	    int b=2*y;
+	    if(a<=b) cout<<a<<endl;
+	    else cout<<b<<endl;
+	}
+	return 0;
+}
