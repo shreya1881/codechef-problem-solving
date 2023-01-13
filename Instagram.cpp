b7ebc49c389b7b6c7880373d1dd06012bf84bb02
shreya1881 <2704shreya@gmail.com>
2023-01-13 09:52:56 +0530

    .

---
+++ Instagram.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    if(x>(10*y)) cout<<"YES\n";
+	    else cout<<"NO\n";
+	}
+	return 0;
+}
