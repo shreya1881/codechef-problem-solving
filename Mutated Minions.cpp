76e18b317bfd4fb28f4a99276005e7dd0b8f535d
shreya1881 <2704shreya@gmail.com>
2023-01-23 19:11:19 +0530

    .

---
+++ Mutated Minions.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,k;
+	    cin>>n>>k;
+	    int arr[n];
+	    for(int i=0; i<n; i++)
+	    cin>>arr[i];
+	    for(int i=0; i<n ;i++)
+	    arr[i]+=k;
+	    int count=0;
+	    for(int i=0; i<n; i++){
+	        if(arr[i]%7==0)
+	        count++;
+	    }
+	    cout<<count<<endl;
+	}
+	return 0;
+}
