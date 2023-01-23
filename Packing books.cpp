0db8f81cc7c6ddd44cda4ca7655156948d7f6bc0
shreya1881 <2704shreya@gmail.com>
2023-01-23 19:04:49 +0530

    .

---
+++ Packing books.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y,z;
+	    cin>>x>>y>>z;
+	     if(y%z==0){
+	        cout<<(y/z)*x<<endl;
+	    }
+	    else{
+	        cout<<((y/z)+1)*x<<endl;
+	    }
+	}
+	return 0;
+}
