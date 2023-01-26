e217e0e7d9e6d3a3e1dac06b3d51ac91cb3fb3f3
shreya1881 <2704shreya@gmail.com>
2023-01-26 09:35:19 +0530

    .

---
+++ Cars and Bikes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    if(n%4==0) cout<<"NO\n";
+	    else cout<<"YES\n";
+	}
+	return 0;
+}
