0aeb648aaacd02d5daa2f898976825e0aee228ad
shreya1881 <2704shreya@gmail.com>
2023-01-31 10:26:04 +0530

    .

---
+++ Is it a vowel or consonant.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	char s;
+	cin>>s;
+	if(s=='A' || s=='E'|| s=='O'|| s=='I'||s=='U')
+	cout<<"Vowel\n";
+	else cout<<"Consonant\n";
+	return 0;
+}
