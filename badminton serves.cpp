22bd6ca1d87e6d00390f99fb77971401b9e41f0c
shreya1881 <2704shreya@gmail.com>
2023-02-09 12:29:39 +0530

    .

---
+++ badminton serves.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+    cin>>t;
+    while(t--)
+    { int a;
+        cin>>a;
+        cout<<((a/2)+1)<<endl;
+    }
+	return 0;
+}
