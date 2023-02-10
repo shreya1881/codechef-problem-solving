b9284135683b2d7b90573f74335c608693a9343c
shreya1881 <2704shreya@gmail.com>
2023-02-10 19:08:28 +0530

    .

---
+++ relativity.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int g,c;
+	    cin>>g>>c;
+	    int x = (c*c/(2*g));
+	    cout<<x<<endl;
+	}
+	return 0;
+}
