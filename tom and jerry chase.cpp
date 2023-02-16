10265768540e0cdad72cfeea863f04bbe2033984
shreya1881 <2704shreya@gmail.com>
2023-02-16 20:34:58 +0530

    .

---
+++ tom and jerry chase.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b;
+	    cin>>a>>b;
+	    if(b>a) cout<<"YES\n";
+	    else cout<<"NO\n";
+	    
+	}
+	return 0;
+}
