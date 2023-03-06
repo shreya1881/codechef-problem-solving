273b1941eecc7aef6442d0e61706a874891c4925
shreya1881 <2704shreya@gmail.com>
2023-03-06 18:33:18 +0530

    .

---
+++ car range.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	while(t--) {
+	    int p, m, v;
+	    cin>>p>>m>>v;
+	    
+	    cout<< (m*v - (p-1)*v) <<endl;
+	}
+	return 0;
+}
