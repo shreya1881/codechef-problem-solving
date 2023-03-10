d78591b08286a4ae08df43b17a1f2e5f0a718fc4
shreya1881 <2704shreya@gmail.com>
2023-03-10 14:00:07 +0530

    .

---
+++ just one more episode.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll x;
+        cin>>x;
+        if(x>24)
+        cout<<"Yes"<<endl;
+        else
+        cout<<"No"<<endl;
+    }
+}
