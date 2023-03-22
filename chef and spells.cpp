752021b31b63f65476d9de608adb8e53643f6148
shreya1881 <2704shreya@gmail.com>
2023-03-22 11:01:20 +0530

    .

---
+++ chef and spells.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int arr[3];
+	    for(int i=0; i<3; i++)
+	    cin>>arr[i];
+	    sort(arr,arr+3);
+	    cout<<arr[1]+arr[2]<<endl;
+	}
+	return 0;
+}
