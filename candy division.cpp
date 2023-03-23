d99bc304a5470167131fa507d62302ee70c06b2f
shreya1881 <2704shreya@gmail.com>
2023-03-23 09:17:37 +0530

    .

---
+++ candy division.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    if(n%3==0) cout<<"Yes\n";
+	    else cout<<"No\n";
+	}
+	return 0;
+}
