06d42651b792abfe624b4a96bfc9506c54e92cb4
shreya1881 <2704shreya@gmail.com>
2023-03-26 19:06:00 +0530

    .

--- snapchat.cpp
+++ snapchat.cpp
@@ -1,27 +1,45 @@
 #include <iostream>
+#include <bits/stdc++.h>
+#include <string.h>
+// #include <stdlib.h>
+// #include <math.h>
 using namespace std;
 
-int main() {
-	// your code goes here
-	int t;
-	cin>>t;
-	while(t--)
-	{
-	    int n;
-	    cin>>n;
-	    int a[n];
-	    int b[n];
-	    for(int i=0; i<n; i++)
-	    cin>>a[i];
-	    for(int i=0; i<n; i++)
-	    cin>>b[i];
-	    int flag=0;
-	    for(int i=0; i<n; i++)
-	    {
-	       if(a[i]!=0 && b[i]!=0)
-	       flag++;
-	    }
-	    cout<<flag<<endl;
-	}
-	return 0;
+// initial solution
+int main()
+{
+    int testcase;
+    cin >> testcase;
+    for (int m = 0; m < testcase; m++)
+    {
+        int n, ans = 0, max = 0;// ans is for temporary streak count, max is for maxm streak count achieved,ans can become 0,max can not
+        cin >> n;
+        int A[n], B[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin>>A[i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            cin>>B[i];
+        }
+        
+
+        for (int i = 0; i < n; i++)// checking loop
+        {
+            if (A[i] > 0 && B[i] > 0)// both sent at least one snap to each other
+            {
+                ans++;
+                if (ans > max)
+                {
+                    max = ans;
+                }
+            }
+            else if (A[i] == 0 || B[i] == 0)// one of them didnt send any snap, breaking the streak
+            {
+                ans = 0;
+            }
+        }
+        cout << max << endl;
+    }
 }
