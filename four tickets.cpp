2bec5852fbc9b24c681a0fada89c4da67a81dfb0
shreya1881 <2704shreya@gmail.com>
2023-04-29 09:38:32 +0530

    .

---
+++ four tickets.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x;
+	    cin>>x;
+	    int a = 4*x;
+	    if(a<=1000)
+	    cout<<"YES\n";
+	    else
+	    cout<<"NO\n";
+	}
+	return 0;
+	
+}
