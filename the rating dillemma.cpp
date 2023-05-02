15e0f929ba28dd365b51928f81ac1bd9feaad0c7
shreya1881 <2704shreya@gmail.com>
2023-05-02 14:03:06 +0530

    .

---
+++ the rating dillemma.cpp
@@ -0,0 +1,38 @@
+/*
+
+Hello coder!!!
+
+Welcome to my solution (hope its correct XD). I just wanted to tell you that  if 
+you want a bit more explanation  about this problem then you can visit my github
+repository where I have posted a brief explanation of answer. 
+
+Feel free to check it out , if you feel that my code can be optimized then
+please create a pull request to update the solution. So in that way it can be 
+helpful for others too.
+
+Thanks and happy coding :)
+
+**my github repo link**
+https://github.com/soniyash9000/Code_chef_Solutions
+
+*/
+
+
+#define ll long long
+#define f(end) for(int i=0;i<end;i++)
+#define e '\n'
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t,n;
+	cin>>t;
+	while(t--)
+	{
+	 cin>>n;
+	 cout<<0-(n+1)<<e;
+	}
+	return 0;
+}
